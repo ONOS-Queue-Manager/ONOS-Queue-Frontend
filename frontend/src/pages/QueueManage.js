d7172f226bf99ba2a2796bf1bd3d59ed5438e8d9
Kavindu Chinthana <119396480+kgchinthana@users.noreply.github.com>
2023-12-26 19:34:29 +0530

    edit table

--- frontend/src/pages/QueueManage.js
+++ frontend/src/pages/QueueManage.js
@@ -1,68 +1,69 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import { useNavigate } from 'react-router-dom'; // Import useHistory from React Router
+import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
-// import "../styles/queueManage.css";
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
+  { field: 'id', headerName: 'ID', width: 130 },
+  { field: 'flow_name', headerName: 'Flow Name', width: 150 },
+  { field: 'table_id', headerName: 'Table ID', width: 150 },
+  { field: 'device', headerName: 'Device', width: 120 },
+  { field: 'device_name', headerName: 'Device Type', width: 150 },
+  { field: 'operational', headerName: 'Operational', width: 120 },
+  { field: 'action', headerName: 'Action', width: 120 },
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 2, flow_name: 'Lannister', table_id: 'Cersei', device: 42, device_name: 'Type B', operational: 'No', action: 'Delete' },
+  { id: 3, flow_name: 'Stark', table_id: 'Arya', device: 16, device_name: 'Type C', operational: 'Yes', action: 'Edit' },
+  { id: 4, flow_name: 'Targaryen', table_id: 'Daenerys', device: null, device_name: 'Type D', operational: 'No', action: 'Delete' },
+  { id: 5, flow_name: 'Melisandre', table_id: null, device: 150, device_name: 'Type E', operational: 'Yes', action: 'Edit' },
+  { id: 6, flow_name: 'Clifford', table_id: 'Ferrara', device: 44, device_name: 'Type F', operational: 'No', action: 'Delete' },
+  { id: 7, flow_name: 'Frances', table_id: 'Rossini', device: 36, device_name: 'Type G', operational: 'Yes', action: 'Edit' },
+  // Add more rows as needed
 ];
 
 const QueueManage = () => {
-  const navigate = useNavigate(); // Create a navigate object
+  const navigate = useNavigate();
 
   const handleInsertClick = () => {
-    // Navigate to the new page (replace '/new-page' with your desired route)
-    navigate('/insertQueue')
+    navigate('/insertQueue');
   };
 
   return (
     <div>
-      <h2 style={{ textAlign: 'center',marginTop: "5%" }}>Flow Table</h2>
+      <h2 style={{ textAlign: 'center', marginTop: '5%' }}>Flow Table</h2>
 
-      <Button variant="contained" color="primary" style={{ marginBottom: '1%', alignItems: 'center', justifyContent: 'center', display: 'flex', marginLeft: '10%' }} onClick={handleInsertClick}>
+      <Button
+        variant="contained"
+        color="primary"
+        style={{
+          marginBottom: '1%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          display: 'flex',
+          marginLeft: '10%',
+        }}
+        onClick={handleInsertClick}
+      >
         Insert
       </Button>
 
-      <div style={{ height: 400, width: '80%', margin: '2% auto', alignItems: 'center', justifyContent: 'center', display: 'flex', marginLeft: '10%' }}>
+      <div
+        style={{
+          height: 400,
+          width: '80%',
+          margin: '2% auto',
+          alignItems: 'center',
+          justifyContent: 'center',
+          display: 'flex',
+          marginLeft: '10%',
+        }}
+      >
         <DataGrid
           rows={rows}
           columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: { page: 0, pageSize: 5 },
-            },
-          }}
-          pageSizeOptions={[5, 10]}
+          pageSize={5}
           checkboxSelection
         />
       </div>
